b51c2861b919c3812b7ab4077d9bee2bc1110dbd
Facqueur <104431014+FacqueurGatien@users.noreply.github.com>
2023-07-11 11:59:15 +0200

    Update main-cpuz.js

--- www/js/main-cpuz.js
+++ www/js/main-cpuz.js
@@ -2,6 +2,8 @@ import { EventClass } from "./EventClass.js";
 import { TableGenerator } from "./Processors/Generator/TableGenerator.js";
 import { ProcessorsCollection } from "./Processors/ProcessorsCollection.js";
 
+let thead = document.querySelectorAll("#thead th")
+
 let proc = new ProcessorsCollection();
 await proc.load();
 EventClass.proc = proc;
@@ -12,3 +14,25 @@ table.generateTable();
 document.getElementById("searchField").addEventListener("input",(e)=>{
     table.generateTable(EventClass.searchArray(e.target.value));
 })
+addSortEvent();
+
+function addSortEvent(){
+    let translate = [
+        // {from:"id",to:"Identifiant"}
+        // ,{from:"brand",to:"Marque"}
+        // ,{from:"family",to:"Famille"}
+        // ,{from:"model",to:"Modèle"}
+        // ,{from:"ghz",to:"Fréquence (Ghz)"},
+        {from:"price",to:"Prix"}
+        // ,{from:"fullName",to:"Nom complet"} 
+    ]
+    for(let t of translate){
+        for(let th of thead){
+            if(t.to==th.textContent){
+                th.addEventListener("click",(e)=>{
+                    table.generateTable(EventClass.sortColumn(t.from));
+                })
+            }
+        }
+    }
+}
