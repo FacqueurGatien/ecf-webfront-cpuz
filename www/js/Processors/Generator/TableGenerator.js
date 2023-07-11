d40307bafbfe16ebdf10b256b8583cb371cdf7d1
Facqueur <104431014+FacqueurGatien@users.noreply.github.com>
2023-07-11 11:58:52 +0200

    move function from tableGenerator to main

--- www/js/Processors/Generator/TableGenerator.js
+++ www/js/Processors/Generator/TableGenerator.js
@@ -5,9 +5,7 @@ class TableGenerator{
         this.proc = _proc;
         this.table = document.getElementById("cpuTable");
         this.tbody = document.getElementById("tbody");
-        this.thead = document.querySelectorAll("#thead th")
         this.nbModel = document.getElementById("nbModel");
-        this.addSortEvent();
     }
     generateTable(array=this.proc.data){
         this.actualData=array;
@@ -28,26 +26,6 @@ class TableGenerator{
         let td = document.createElement("td")
         td.textContent = spec;
         return td;
-    }
-    addSortEvent(){
-        let translate = [
-            // {from:"id",to:"Identifiant"}
-            // ,{from:"brand",to:"Marque"}
-            // ,{from:"family",to:"Famille"}
-            // ,{from:"model",to:"Modèle"}
-            // ,{from:"ghz",to:"Fréquence (Ghz)"},
-            {from:"price",to:"Prix"}
-            // ,{from:"fullName",to:"Nom complet"} 
-        ]
-        for(let t of translate){
-            for(let th of this.thead){
-                if(t.to==th.textContent){
-                    th.addEventListener("click",(e)=>{
-                        this.generateTable(EventClass.sortColumn(t.from));
-                    })
-                }
-            }
-        }
     }
 }
 export {TableGenerator}
